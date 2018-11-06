2fc4136a9851dfd9b0832621d016719d2e68cc66
Giorgio Sironi <g.sironi@elifesciences.org>
2018-11-06 14:24:41 +0000

    Add role for users to access via kubectl

---
+++ user-roles.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_role" "kubernetes--demo--user" {
+  name = "kubernetes--demo--AmazonEKSUserRole"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": { "AWS": "arn:aws:iam::512686554592:user/elife-alfred" },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+#  what this role can do is stored in the aws-auth ConfigMap in kube-system namespace
